b8683187b9ee76a4a90f7c1994976b0c041d35ca
dmitrymartynenko2000-star <dmitrymartynenko2000@gmail.com>
2025-10-19 11:42:12 +0300

    Update data.py

--- data.py
+++ data.py
@@ -3,12 +3,12 @@ import pandas as pd
 def get_data():
     dishes = [
         # name, category, diet, calories, proteins, fats, carbs, tags, image_url, recommendations
-        ("Курица с овощами", "горячее", "обычное", 450, 35, 14, 40, "курица,без свинины,без остро", "https://img.freepik.com/free-photo/grilled-chicken-with-vegetables_140725-1687.jpg", "Салат Цезарь,Омлет с овощами"),
-        ("Рыба на пару", "горячее", "диетическое", 220, 28, 6, 2, "рыба,легкое,без глютена", "https://img.freepik.com/free-photo/steamed-fish-with-herbs_1203-3583.jpg", "Гречка с мясом,Салат Цезарь"),
-        ("Гречка с мясом", "горячее", "сытное", 520, 25, 12, 70, "говядина,сытно", "https://img.freepik.com/free-photo/buckwheat-with-meat-vegetables_140725-1623.jpg", "Рыба на пару,Омлет с овощами"),
-        ("Омлет с овощами", "завтрак", "вегетарианское", 300, 18, 18, 8, "омлет,овощи", "https://img.freepik.com/free-photo/fluffy-omelette-with-vegetables_140725-1536.jpg", "Курица с овощами,Салат Цезарь"),
-        ("Салат Цезарь", "салат", "обычное", 380, 24, 22, 20, "курица,салат", "https://img.freepik.com/free-photo/fresh-caesar-salad-with-chicken_140725-1582.jpg", "Курица с овощами,Паста с томатами"),
-        ("Паста с томатами", "горячее", "вегетарианское", 430, 14, 12, 62, "паста,без свинины", "https://img.freepik.com/free-photo/pasta-with-tomato-sauce_140725-1468.jpg", "Салат Цезарь,Рыба на пару"),
+        ("Курица с овощами", "горячее", "обычное", 450, 35, 14, 40, "курица,без свинины,без остро", "https://img.iamcook.ru/old/upl/recipes/cat/u1378-b97469a2dddc017b00c8aacc4957cf14.jpg", "Салат Цезарь,Омлет с овощами"),
+        ("Рыба на пару", "горячее", "диетическое", 220, 28, 6, 2, "рыба,легкое,без глютена", "https://prostokvashino.ru/upload/resize_cache/iblock/370/800_800_0/3706c5d808e12659543fe4306c52eb23.jpg", "Гречка с мясом,Салат Цезарь"),
+        ("Гречка с мясом", "горячее", "сытное", 520, 25, 12, 70, "говядина,сытно", "https://img.povar.ru/mobile/5a/cc/22/2d/grechka_s_myasom-868085.JPG", "Рыба на пару,Омлет с овощами"),
+        ("Омлет с овощами", "завтрак", "вегетарианское", 300, 18, 18, 8, "омлет,овощи", "https://images.gastronom.ru/HGWEXxM5PcNnoMdU5TdvCHfTsApQ7XJflntpnlJBTwU/pr:recipe-cover-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzLzY3YzMyNjNlLTRiYzItNDcyNC1iYTkwLTdmOTY3NmI1YjRhNy5qcGc.webp", "Курица с овощами,Салат Цезарь"),
+        ("Салат Цезарь", "салат", "обычное", 380, 24, 22, 20, "курица,салат", "https://static.1000.menu/img/content-v2/eb/79/19516/salat-cezar-klassicheskii-s-kuricei_1611309331_16_max.jpg", "Курица с овощами,Паста с томатами"),
+        ("Паста с томатами", "горячее", "вегетарианское", 430, 14, 12, 62, "паста,без свинины", "https://lifehacker.ru/wp-content/uploads/2020/01/shutterstock_1315335506-1_1589978896.jpg", "Салат Цезарь,Рыба на пару"),
     ]
     df = pd.DataFrame(dishes, columns=[
         "name", "category", "diet", "calories", "proteins", "fats", "carbs", "tags", "image_url", "recommendations"
